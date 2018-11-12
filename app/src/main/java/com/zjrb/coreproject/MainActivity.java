6566f982a8ebf98babb8373863ecfb017300d708
a_liYa <liyaf@8531.cn>
2018-11-12 10:48:03 +0800

    删除 ILoad 接口

---
+++ app/src/main/java/com/zjrb/coreproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zjrb.coreproject;
+
+import android.os.Bundle;
+
+import com.zjrb.core.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
