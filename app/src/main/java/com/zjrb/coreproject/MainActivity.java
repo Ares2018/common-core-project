40d3878cbae801f41ceb5a6b50dceed7dfab9e0b
李欣科 <lixink@8531.cn>
2019-04-09 14:45:07 +0800

    修改测试代码

--- app/src/main/java/com/zjrb/coreproject/MainActivity.java
+++ app/src/main/java/com/zjrb/coreproject/MainActivity.java
@@ -1,14 +1,37 @@
 package com.zjrb.coreproject;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.zjrb.core.base.BaseActivity;
+import com.zjrb.core.load.LoadingIndicatorDialog;
 
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final LoadingIndicatorDialog loadingIndicatorDialog=new LoadingIndicatorDialog(this);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingIndicatorDialog.show();
+                loadingIndicatorDialog.setToastText("加载中...");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadingIndicatorDialog.finish(true);
+                    }
+                },1000);
+            }
+        });
+
+
+
+
     }
 
 }
