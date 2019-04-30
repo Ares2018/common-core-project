f5ae845fb517ac0cfdf1c3c4a245307dd289f06c
李欣科 <lixink@8531.cn>
2019-04-30 15:21:42 +0800

    功能引导

--- core/src/main/java/com/zjrb/core/ui/widget/GuideView.java
+++ core/src/main/java/com/zjrb/core/ui/widget/GuideView.java
@@ -283,8 +283,7 @@ public class GuideView extends FrameLayout {
     }
 
     private static boolean isShowGuide(String tag) {
-//        return SPHelper.get().get(tag, true);
-        return true;
+        return SPHelper.get().get(tag, true);
     }
 
     private static void hideGuide(String tag) {
